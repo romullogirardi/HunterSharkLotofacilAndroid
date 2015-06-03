220929f11ff3702ebb79b1b2f7d72b9877e650d6
girardi <romullogirardi@gmail.com>
2015-06-03 15:57:30 -0300

    Ajustes nos arquivos de layout.

--- gen/com/romullogirardi/huntsharkandroid/R.java
+++ gen/com/romullogirardi/huntsharkandroid/R.java
@@ -12,22 +12,40 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_launcher_shark=0x7f020001;
     }
     public static final class id {
-        public static final int image_view_bet=0x7f06000c;
+        public static final int edit_text_city=0x7f060019;
+        public static final int edit_text_reward_11_points=0x7f06001e;
+        public static final int edit_text_reward_12_points=0x7f06001d;
+        public static final int edit_text_reward_13_points=0x7f06001c;
+        public static final int edit_text_reward_14_points=0x7f06001b;
+        public static final int edit_text_reward_15_points=0x7f06001a;
         public static final int list_view_contests=0x7f060000;
-        public static final int text_view_bet_investment=0x7f060005;
-        public static final int text_view_bet_profit=0x7f060006;
-        public static final int text_view_bet_reward=0x7f060004;
-        public static final int text_view_contest_id=0x7f060007;
-        public static final int text_view_contest_jogo1=0x7f060008;
-        public static final int text_view_contest_jogo2=0x7f060009;
-        public static final int text_view_contest_jogo3=0x7f06000a;
-        public static final int text_view_contest_jogo4=0x7f06000b;
-        public static final int text_view_recommended_investment=0x7f060002;
-        public static final int text_view_recommended_profit=0x7f060003;
-        public static final int text_view_recommended_reward=0x7f060001;
-        public static final int toogle_button0=0x7f06000d;
+        public static final int text_view_bet_investment=0x7f060002;
+        public static final int text_view_bet_profit=0x7f060003;
+        public static final int text_view_bet_reward=0x7f060001;
+        public static final int text_view_contest=0x7f060009;
+        public static final int text_view_contest_id=0x7f060004;
+        public static final int text_view_contest_jogo1=0x7f060005;
+        public static final int text_view_contest_jogo2=0x7f060006;
+        public static final int text_view_contest_jogo3=0x7f060007;
+        public static final int text_view_contest_jogo4=0x7f060008;
+        public static final int toogle_button1=0x7f06000a;
+        public static final int toogle_button10=0x7f060013;
+        public static final int toogle_button11=0x7f060014;
+        public static final int toogle_button12=0x7f060015;
+        public static final int toogle_button13=0x7f060016;
+        public static final int toogle_button14=0x7f060017;
+        public static final int toogle_button15=0x7f060018;
+        public static final int toogle_button2=0x7f06000b;
+        public static final int toogle_button3=0x7f06000c;
+        public static final int toogle_button4=0x7f06000d;
+        public static final int toogle_button5=0x7f06000e;
+        public static final int toogle_button6=0x7f06000f;
+        public static final int toogle_button7=0x7f060010;
+        public static final int toogle_button8=0x7f060011;
+        public static final int toogle_button9=0x7f060012;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -36,13 +54,20 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int cidade_dois_pontos=0x7f04000a;
         public static final int concurso=0x7f040004;
+        public static final int concurso_dois_pontos=0x7f040009;
         public static final int investimento_dois_pontos=0x7f040002;
         public static final int jogo1=0x7f040005;
         public static final int jogo2=0x7f040006;
         public static final int jogo3=0x7f040007;
         public static final int jogo4=0x7f040008;
         public static final int lucro_dois_pontos=0x7f040003;
+        public static final int premio_11_pontos=0x7f04000f;
+        public static final int premio_12_pontos=0x7f04000e;
+        public static final int premio_13_pontos=0x7f04000d;
+        public static final int premio_14_pontos=0x7f04000c;
+        public static final int premio_15_pontos=0x7f04000b;
         public static final int recompensa_dois_pontos=0x7f040001;
     }
     public static final class style {
--- src/com/romullogirardi/huntsharkandroid/view/MainActivity.java
+++ src/com/romullogirardi/huntsharkandroid/view/MainActivity.java
@@ -1,7 +1,16 @@
 package com.romullogirardi.huntsharkandroid.view;
 
+import com.romullogirardi.huntsharkandroid.R;
+
 import android.app.Activity;
+import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+	//MÉTODOS DO CICLO DE VIDA DA Activity
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 }
